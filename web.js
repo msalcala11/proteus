281dc1ce41b430d964e69b38ae8147e0b113ddb3
Marty Alcala <msalcala11@gmail.com>
2013-08-11 23:04:41 +0000

    added web.js

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
+
+//Dummy comment to activate change
 var express = require('express');
 
+var number = 2;
 var app = express.createServer(express.logger());
 
+app.use(express.static(process.env.PWD + '/'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var fs = require("fs");
+  var buffer = new Buffer(fs.readFileSync("index.html"));
+  var indexStr = buffer.toString("utf-8",0,buffer.length);
+  response.send(indexStr);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
