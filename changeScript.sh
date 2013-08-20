672450df8fba0bda8b2baa56f2ce5a5b5f0a9db3
Marty Alcala <msalcala11@gmail.com>
2013-08-20 23:52:42 +0000

    added new line characters to what is echoed by the shell script to make the stdout more readable

--- changeScript.sh
+++ changeScript.sh
@@ -3,24 +3,31 @@
 echo "Adding files to git.."
 git add --all
 
-echo "Enter a description for the commit: "
+echo "
+Enter a description for the commit: "
 read commitMessage
 
-echo "Committing changes.."
+echo "
+Committing changes.."
 git commit -m "$commitMessage"
 
-echo "Pushing to GitHub develop branch.."
+echo "
+Pushing to GitHub develop branch.."
 git push origin develop
 
-echo "Switching to staging branch.."
+echo "
+Switching to staging branch.."
 git checkout staging
 
-echo "Merging staging branch with develop branch.."
+echo "
+Merging staging branch with develop branch.."
 git merge develop
 
-echo "Pushing staging branch to GitHub.."
+echo "
+Pushing staging branch to GitHub.."
 git push origin staging
 
-echo "Pushing staging branch to Heroku.."
+echo "
+Pushing staging branch to Heroku.."
 git push staging-heroku staging:master
 
