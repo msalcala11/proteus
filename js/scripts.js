11d384f68911b928b4a6c9c56e83abf1018b1791
Marty Alcala <msalcala11@gmail.com>
2013-08-20 22:08:17 +0000

    improved valdation for email form

--- js/scripts.js
+++ js/scripts.js
@@ -41,16 +41,39 @@ $("#subscribe-input").keyup(function(event){
     $("#btn-subscribe").click(function(e){
 	//    alert("Thank you for subscribing.");
 
-	var input = $("#subscribe-input").val();
-	if(input.search("@") == -1){
+
+if(!$("#btn-subscribe").hasClass('disabled')){//do not do anything if already submitted email address
+    var input = $("#subscribe-input").val();
+    function validateEmail(email) {// http://thejimgaudet.com/articles/support/web/jquery-email-validation-without-a-plugin/
+	if(email == ""){
+	    var isEmail = false;
+	    }
+	else{
+	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+//	    console.log("return of validateEmail: " + emailReg.test(input));
+	    var isEmail = emailReg.test(input);
+	    }
+	return isEmail;
+    }
+
+	
+    //var email = jQuery("#input[type='email']").val();
+	if(!validateEmail(input)){
 	    message = "Please enter a valid email address.";
 	    }
 	else{
 	    message = "Thank you for subscribing!";
+	    $('#btn-subscribe').removeClass('btn-primary');
+	    $('#btn-subscribe').addClass('disabled');
+	    $('#btn-subscribe').addClass('btn-success');
+	    $('#btn-subscribe').text("Success!");
+	    $('#subscribe-input').prop('disabled', true);
 	    }
-	console.log(message);
+//	console.log(message);
 	$("#subscribe-response p").text(message);
 	$("#subscribe-response").removeClass("hidden");
+}
+	
     });
 //}
 //});
