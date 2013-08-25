e2df8e21323f7c0ca6323eb0383392fced93a437
Marty Alcala <msalcala11@gmail.com>
2013-08-25 18:36:36 +0000

    cleaned up scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,72 @@
 //These are my js client scripts
 
 //This segment changes the active navbar button when it is clicked
-	  $(document).ready(function () {
-            $('ul.nav > li').click(function (e) {
-	      var h = $('a', this).attr('href');
-	      
-              $('ul.nav > li').removeClass('active');
-              $(this).addClass('active');                
-      
-      //now lets scroll to the appropriate section
-          if(h.indexOf('#') === 0) {
+$(document).ready(function () {
+    $('ul.nav > li').click(function (e) {
+	var h = $('a', this).attr('href');	
+        $('ul.nav > li').removeClass('active');
+        $(this).addClass('active');                
+	
+	//now lets scroll to the appropriate section
+        if(h.indexOf('#') === 0) {
             h = h.substring(1);
             if(h === 'home') {
-              $('html, body').stop().animate({
-                scrollTop: 0
-              });
+		$('html, body').stop().animate({
+                    scrollTop: 0
+		});
             } else {
-              $('html, body').stop().animate({
-                scrollTop: $('a[name="' + h + '"]').offset().top -80
-              });
+		$('html, body').stop().animate({
+                    scrollTop: $('a[name="' + h + '"]').offset().top -80
+		});
             }
             return false;
-          }
-          return true;
-        
-
-            });            
-	  });
-
-//script for monthly newsletter subscription click
-//$(document).ready(function () {
-//function validateSubscription(){
-/*$("#subscribe-input").keyup(function(event){
-    if(event.keyCode === 13){
-        $("#btn-subscribe").click();
-    }
-});*/
+        }
+        return true;	
+    });            
+});
 
 
-    $("#newsletter-form").submit(function(e){
-	//    alert("Thank you for subscribing.");
+//Lets handle the monthly newsletter subscription
+$("#newsletter-form").submit(function(e){
 
-
-if(!$("#btn-subscribe").hasClass('disabled')){//do not do anything if already submitted email address
-    var input = $("#subscribe-input").val();
-    function validateEmail(email) {// http://thejimgaudet.com/articles/support/web/jquery-email-validation-without-a-plugin/
-	if(email === ""){
-	    var isEmail = false;
+    if(!$("#btn-subscribe").hasClass('disabled')){//do not do anything if already submitted email address
+	var input = $("#subscribe-input").val();
+	function validateEmail(email) {// http://thejimgaudet.com/articles/support/web/jquery-email-validation-without-a-plugin/
+	    if(email === ""){
+		var isEmail = false;
 	    }
-	else{
-	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-//	    console.log("return of validateEmail: " + emailReg.test(input));
-	    var isEmail = emailReg.test(input);
+	    else{
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		var isEmail = emailReg.test(input);
 	    }
-	return isEmail;
-    }
-
+	    return isEmail;
+	}
 	
-    //var email = jQuery("#input[type='email']").val();
 	if(!validateEmail(input)){
 	    message = "Please enter a valid email address.";
-	    }
+	}
 	else{
 	    //lets post the email and store it in db
 	    $.ajax({  
 		type: "POST",  
 		url: "/new_newsletter_subscriber",  
 		data: {"email":input},  
-		//contentType: "application/json",
-		//dataType: "json",
-		//data: "{'name':'" + name + "', 'age':'" + age + "'}",
 		success: function() {  
 		    //we implement the success action below  
 		}  
 	    });  
-
+	    
 	    message = "Thank you for subscribing!";
 	    $('#btn-subscribe').removeClass('btn-primary');
 	    $('#btn-subscribe').addClass('disabled');
 	    $('#btn-subscribe').addClass('btn-success');
 	    $('#btn-subscribe').text("Success!");
 	    $('#subscribe-input').prop('disabled', true);
-	    }
-//	console.log(message);
+	}
 	$("#subscribe-response p").text(message);
 	$("#subscribe-response").removeClass("hidden");
-}
-	e.preventDefault();
-    });
-//}
-//});
+    }
+    e.preventDefault();
+});
+
 
